ec442433ab971b1fe9a3ad9c2c0117e3c3e88922
aristidesz <aristideszenonos@gmail.com>
2014-12-13 20:26:53 +0000

    My message Mew

---
+++ app/src/main/java/com/example/aristidesz/draft/MainScreen.java
@@ -0,0 +1,37 @@
+package com.example.aristidesz.draft;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * Created by AristidesZ on 13/12/2014.
+ */
+public class MainScreen extends Activity{
+
+    private Button buttonMap, buttonList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setButtons();
+
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent (MainScreen.this, ListVillages.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    private void setButtons() {
+        buttonMap = (Button) findViewById(R.id.btMainMap);
+        buttonList = (Button) findViewById(R.id.btMainList);
+    }
+}
